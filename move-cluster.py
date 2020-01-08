bc592b41e7d4783ba526c4bd778296f3fd96ecd8
Dmitry Novik <dmitry.novik@gmail.com>
2020-01-08 13:29:23 +1100

    parsing command line arguments

--- move-cluster.py
+++ move-cluster.py
@@ -1,15 +1,40 @@
 import sys, getopt
 
-source_url = None
-source_user = None
-source_pass = None
-dest_url = None
-dest_user = None
-dest_pass = None
+print("\nRabbitMQ Blue-Green Migration Script\n")
 
-opts, arg = getopt.getopt(sys.argv[1:], "s:d")
-print("arguments:")
+from_cluster = None
+from_user = "admin"
+from_password = "admin"
+to_cluster = None
+to_user = "admin"
+to_password = "admin"
+
+opts, arg = getopt.getopt(sys.argv[1:], "", 
+    ["from-cluster=", "from-user=", "from-password=", "to-cluster=", "to-user=", "to-password="])
+
+print("received arguments:")
 for opt, arg in opts:
     print("\t", opt, " -> ", arg)
+    if opt == "--from-cluster":
+        from_cluster = arg
+    elif opt == "--from-user":
+        from_user = arg
+    elif opt == "--from-password":
+        from_password = arg
+    elif opt == "--to-cluster":
+        to_cluster = arg
+    elif opt == "--to-user":
+        to_user = arg
+    elif opt == "--to-password":
+        to_password = arg
+    
+if from_cluster == None:
+    raise Exception("Missing argument: --from-cluster")
+if to_cluster == None:
+    raise Exception("Missing argument: --to-cluster")
+if to_cluster == from_cluster:
+    raise Exception("Same source and destination, cannot proceed:", to_cluster)
 
+print("\nSource cluster (url, user, pass):", from_cluster, from_user, from_password)
+print("Destination cluster (url, user, pass):", to_cluster, to_user, to_password)
 
