dad9465f7d109da1103cfc03106113fdb4d00dd3
Dmitry Novik <dmitry.novik@gmail.com>
2020-01-08 16:38:14 +1100

    export defs

--- move-cluster.py
+++ move-cluster.py
@@ -1,4 +1,4 @@
-import sys, getopt
+import sys, getopt, subprocess
 
 print("\nRabbitMQ Blue-Green Migration Script\n")
 
@@ -9,6 +9,7 @@ to_cluster = None
 to_user = "admin"
 to_password = "admin"
 
+# Parse Command Line Arguments:
 opts, arg = getopt.getopt(sys.argv[1:], "", 
     ["from-cluster=", "from-user=", "from-password=", "to-cluster=", "to-user=", "to-password="])
 
@@ -37,4 +38,12 @@ if to_cluster == from_cluster:
 
 print("\nSource cluster (url, user, pass):", from_cluster, from_user, from_password)
 print("Destination cluster (url, user, pass):", to_cluster, to_user, to_password)
+
+# export definitions:
+defs_path = "rabbitmq.definitions.json"
+export_return_code = subprocess.call(
+    ['python', './rabbitmqadmin', '-H', from_cluster, '-u', from_user, '-p', from_password, 'export', defs_path])
+
+if export_return_code != 0:
+    raise Exception('Failed to export definitions')
 
